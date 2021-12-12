4ad3aa802c05a9a3b3102afa4d696501e0e5f33e
maxsunnyday <max.sun@yale.edu>
2021-12-11 23:16:31 -0500

    initilized functions

---
+++ santorini_game.py
@@ -0,0 +1,17 @@
+class Board:
+    def __init__(self, *args):
+        super(Board, self).__init__(*args)
+        
+        
+
+class Slot:
+    def __init__(self, *args):
+        super(Slot, self).__init__(*args)
+
+class Worker:
+    def __init__(self, *args):
+        super(Worker, self).__init__(*args)
+
+class Player:
+    def __init__(self, *args):
+        super(Player, self).__init__(*args)
