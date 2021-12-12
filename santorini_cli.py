ce5d5aef6ada2200584efc0c259bb28633b0cd64
maxsunnyday <max.sun@yale.edu>
2021-12-12 16:54:31 -0500

    added workers to board

--- santorini_cli.py
+++ santorini_cli.py
@@ -3,33 +3,44 @@ from santorini_game import *
 class SantoriniCLI:
     """Display board and prompt a move"""
     def __init__(self):
-        self.game = Game()
-        self.white_player = Player()
-        self.black_player = Player()
+        self.game = Game('human', 'human')
             
     def run(self):
-            while True:
-                try:
-                    amount = input("Amount?\n>")
-                    break
-                except InvalidOperation:
-                    print("Please try again with a valid dollar amount.")
+        while True:
+            print(self.game.board)
+            # fetch current turn number
+            curr_turn = self.game.get_turn()
+            # if odd, white moves. if even, blue moves.
+            if (curr_turn % 2) != 0:
+                print("Turn: {}, white (AB)".format(curr_turn))
+                white_turn()
+            else:
+                print("Turn: {}, blue (AB)".format(curr_turn))
+                blue_turn()
+            
 
             while True:
                 try:
-                    date = input("Date? (YYYY-MM-DD)\n>")
-                    date = datetime.strptime(date, "%Y-%m-%d").date()
+                    move = input("Select a direction to move (n, ne, e, se, s, sw, w, nw)\n")
                     break
                 except ValueError:
                     print("Please try again with a valid date in the format YYYY-MM-DD.")
 
             while True:
                 try:
-                    date = input("Date? (YYYY-MM-DD)\n>")
-                    date = datetime.strptime(date, "%Y-%m-%d").date()
+                    build = input("Select a direction to build (n, ne, e, se, s, sw, w, nw)\n")
                     break
                 except ValueError:
                     print("Please try again with a valid date in the format YYYY-MM-DD.")
+
+    def white_turn():
+                while True:
+                    try:
+                        worker_input = input("Select a worker to move\n")
+                        if worker_input not "A"
+                        break
+                    except ValueError:
+                        print("Please try again with a valid date in the format YYYY-MM-DD.")
         
 if __name__ == "__main__":
     SantoriniCLI().run()
--- santorini_game.py
+++ santorini_game.py
@@ -1,8 +1,15 @@
 import math
 
 class Game:
-    def __init__(self):
+    def __init__(self, player1_type, player2_type):
         self.board = Board()
+        self.white_player = Player("w", self.board, player1_type)
+        self.black_player = Player("b", self.board, player2_type)
+        self.curr_turn = 1
+        self.furthest_turn = 1
+
+    def get_turn(self):
+        return self._curr_turn
 
 class Board:
     def __init__(self):
@@ -16,10 +23,13 @@ class Board:
 
     def __repr__(self):
         return_string = ""
-        board_separator = "+--+--+--+--+--+\n"
+        board_separator = "+--+--+--+--+--+"
+        new_line = "\n"
         return_string += board_separator
 
         for row in self.slots:
+            return_string += new_line
+            
             for s in row:
                 return_string += "|"
                 return_string += s.__repr__()
@@ -31,11 +41,11 @@ class Board:
 
 
 class Slot:
-    def __init__(self, row, col):
+    def __init__(self, row, col, worker = 0):
         self.row = row + 1
         self.col = col + 1
         self.level = 0
-        self.worker = 0
+        self.worker = worker
 
     def __repr__(self):
         if self.worker == 0:
@@ -44,12 +54,34 @@ class Slot:
             return f"{self.level}{self.worker}"
 
 class Worker:
-    def __init__(self):
-        self.position = []
+    def __init__(self, letter, slot):
+        self.name = letter
+        self.slot = slot
+
+    def __repr__(self):
+        return self.name
 
 class Player:
-    def __init__(self):
-        self.workers = []
+    def __init__(self, color, board, player_type):
+        self.color = color
+        self.player_type = player_type
 
-board = Board()  
-print(board)
+        if color == "w":
+            self.workers = []
+            w1 = Worker("A", board.slots[3][1])
+            self.workers.append(w1)
+            board.slots[3][1].worker = w1
+            w2 = Worker("B", board.slots[1][3])
+            self.workers.append(w2)
+            board.slots[1][3].worker = w2
+        else:
+            self.workers = []
+            w1 = Worker("Y", board.slots[1][1])
+            self.workers.append(w1)
+            board.slots[1][1].worker = w1
+            w2 = Worker("Z", board.slots[3][3])
+            self.workers.append(w2)
+            board.slots[3][3].worker = w2
+
+# board = Board()  
+# print(board)
